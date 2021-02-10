fc8f00b7de9db8a896c35aa72d601e6e65a3f302
Thomas Stromberg <t+github@stromberg.org>
2021-02-09 21:53:05 -0800

    kconfsync refactor

--- cmd/prepare-node/prep-kconfsync.sh
+++ cmd/prepare-node/prep-kconfsync.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #
-# Prepare node for ansible
+# Prepare node for confsync
 set -eux
 
 repo="host-$(hostname -s)"
+srcdir="$(dirname $0)"
 cat /dev/zero | ssh-keygen -q -N ""
 git config --global user.email "${repo}@kernel.cafe"
 git config --global user.name "${repo}"
 
 tf=$(mktemp)
-repo="host-$(hostname -s)"
 echo "*/5 * * * * $HOME/${repo}/sync.sh" > $tf
 crontab $tf
 rm -f $tf
@@ -19,7 +19,12 @@ cat $HOME/.ssh/id_rsa.pub
 read nada
 
 echo "MANUAL STEP: Create ${repo} repository on GitHub"
+read nadab
+
 cd $HOME
 git checkout git@github.com:KernelCafe/${repo}.git
 cd "${repo}"
+cp "${srcdir}/../kconfsync/kconfsync.sh" sync.sh
+cp "${srcdir}/../kconfsync/gitignore" .gitignore
+chmod 755 sync.sh
 ./sync.sh
