dd2a8732e8401c6d767d4fa3dc411f23a94afb58
Thomas Stromberg <t+github@stromberg.org>
2021-02-09 22:04:02 -0800

    Remove cwd assumptions

--- cmd/prepare-node/prep-kconfsync.sh
+++ cmd/prepare-node/prep-kconfsync.sh
@@ -24,7 +24,8 @@ echo "MANUAL STEP: Create ${repo} repository on GitHub"
 read nadab
 
 git clone git@github.com:KernelCafe/${repo}.git "${HOME}/${repo}"
-cp "${srcdir}/../kconfsync/kconfsync.sh" sync.sh
-cp "${srcdir}/../kconfsync/gitignore" .gitignore
-chmod 755 sync.sh
+cp "${srcdir}/../kconfsync/kconfsync.sh" "${HOME}/${repo}/sync.sh"
+cp "${srcdir}/../kconfsync/gitignore" "${HOME}/${repo}/.gitignore"
+chmod 755 "${HOME}/${repo}/sync.sh"
+cd "${HOME}/${repo}"
 ./sync.sh
