84bc5098c883928eef64ed6f9f8084e7dd7e87ff
Thomas Stromberg <t+kernelcafe@stromberg.org>
2021-03-03 11:32:04 -0800

    Include static playbooks

--- ansible/update.sh
+++ ansible/update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 w="${HOME}/welcome"
-pb="${HOME}/ansible/playbooks/auto"
+pb="${HOME}/ansible/run"
+
 mkdir -p $pb
 cd $HOME/automation/ansible
 
 git fetch && git pull
 cp ansible.cfg $HOME/ansible
-
 cwd=$(pwd)
 
 
@@ -16,6 +16,7 @@ cd ..
 
 killall ansible
 rm -f $pb/*.yaml
+cp $HOME/automation/ansible/playbooks/* $pb
 go run cmd/generate-ansible/main.go --usermap $w/auth/users.yaml --groupmap $w/auth/groups.yaml --nodemap $w/nodes/nodes.yaml --out $pb
 egrep -o -- "- name:.*"  $w/nodes/nodes.yaml | cut -d " " -f3 > $HOME/ansible/hosts
 ls $pb/* | xargs -n1 -P24 ansible-playbook -i $HOME/ansible/hosts
